389c2e8d72771cf4eec879fdf86d080d3fcaa2da
malt9830 <malt9830@stud.kea.dk>
2023-12-24 03:08:10 +0100

    Updated js files to ts

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import AutoImport from "unplugin-auto-import/vite";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({}), AutoImport({ imports: ["vue"] })],
+  plugins: [vue(), Components({}), AutoImport({ imports: ['vue'], dts: './auto-imports.d.ts' })],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
