4ef15c141343bdee598e60e21651cfcb448c79e0
malt9830 <malt9830@stud.kea.dk>
2023-12-24 02:55:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="bg-zinc-900 h-screen grid place-content-center">
+    <p class="text-2xl text-white text-center mb-4">Lesgooo</p>
+    <img  class="animate-pulse" src="./assets/logo.svg" alt="Vue logo" width="125" height="125" />
+  </main>
+</template>
+
+<script setup>
+</script>
