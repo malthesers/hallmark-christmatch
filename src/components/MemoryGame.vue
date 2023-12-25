87c959b997e596d8fb0ddc80061c78473a30c7cc
malt9830 <malt9830@stud.kea.dk>
2023-12-25 02:58:49 +0100

    Prevents playing the game while reseting game

--- src/components/MemoryGame.vue
+++ src/components/MemoryGame.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="flex flex-col">
     <p class="text-center text-2xl mb-2">{{ guesses }}</p>
-    <button @click="startGame" class="mx-auto text-2xl border-2 border-white px-3 py-2 rounded-lg">New Game</button>
-    <div class="max-w-2xl grid grid-cols-5 gap-4 p-4">
+    <button @click="startGame(1000)" class="mx-auto text-2xl border-2 border-white px-3 py-2 rounded-lg">New Game</button>
+    <div :class="{ 'pointer-events-none': loading }" class="max-w-2xl grid grid-cols-5 gap-4 p-4">
       <MovieCard v-for="movie in memoryMovies" :movie="movie" :firstMovie="firstMovie" :secondMovie="secondMovie"
         :guessedMovies="guessedMovies" @click="selectMovie(movie)" />
     </div>
@@ -13,6 +13,8 @@
 import { shuffle } from 'lodash'
 import { allMovies } from '@/data/movies';
 import type { Movie } from '@/types/movie';
+
+const loading: Ref<boolean> = ref(true)
 
 const movies: Ref<Movie[]> = ref(allMovies)
 const memoryMovies: Ref<Movie[]> = ref([])
@@ -53,17 +55,24 @@ function selectMovie(movie: Movie): void {
   }
 }
 
-function startGame(): void {
+function startGame(delay: number = 0): void {
+  // Set loading to true
+  loading.value = true
+
   // Clear game states
   firstMovie.value = null
   secondMovie.value = null
   guessedMovies.value = []
   guesses.value = 0
 
-  // Shuffles array and cuts to first 10 duplicating each of those into another shuffled array adding id based on index
-  memoryMovies.value = shuffle(shuffle(movies.value).slice(0, 10).flatMap(movie => [movie, movie])).map((movie, index) => ({ ...movie, id: index }))
-
-  console.log('start', memoryMovies.value)
+  // Create timeout to change movies after flip animation is done
+  setTimeout(() => {
+    // Shuffles array and cuts to first 10 duplicating each of those into another shuffled array adding id based on index
+    memoryMovies.value = shuffle(shuffle(movies.value).slice(0, 10).flatMap(movie => [movie, movie])).map((movie, index) => ({ ...movie, id: index }))
+    // Sets loading state to false after update
+    loading.value = false
+    console.log('start', memoryMovies.value)
+  }, delay)
 }
 
 onMounted(() => startGame())
