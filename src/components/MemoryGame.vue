13005862b94e24fc747f321aa08ce41294e9701a
malt9830 <malt9830@stud.kea.dk>
2023-12-25 02:36:26 +0100

    Now reveals both movies then hides them on click

--- src/components/MemoryGame.vue
+++ src/components/MemoryGame.vue
@@ -2,8 +2,8 @@
   <section>
     <p class="text-center text-2xl">{{ guesses }}</p>
     <div class="max-w-2xl grid grid-cols-5 gap-4 p-4">
-      <MovieCard v-for="movie in memoryMovies" :movie="movie" :chosenMovie="chosenMovie" :guessedMovies="guessedMovies"
-        @click="selectMovie(movie)" />
+      <MovieCard v-for="movie in memoryMovies" :movie="movie" :chosenMovie="chosenMovie" :secondMovie="secondMovie"
+        :guessedMovies="guessedMovies" @click="selectMovie(movie)" />
     </div>
   </section>
 </template>
@@ -15,7 +15,10 @@ import type { Movie } from '@/types/movie';
 
 const movies: Ref<Movie[]> = ref(allMovies)
 const memoryMovies: Ref<Movie[]> = ref([])
+
 const chosenMovie: Ref<Movie | null> = ref(null)
+const secondMovie: Ref<Movie | null> = ref(null)
+
 const guessedMovies: Ref<string[]> = ref([])
 const guesses: Ref<number> = ref(0)
 
@@ -25,20 +28,24 @@ function selectMovie(movie: Movie): void {
     chosenMovie.value = movie
   }
 
+  else if (secondMovie.value) {
+    chosenMovie.value = null
+    secondMovie.value = null
+  }
+
   // If a movie is already chosen and a different movie was clicked
   else if (chosenMovie.value.id !== movie.id) {
+    secondMovie.value = movie
     guesses.value++
 
     // If correct second movie, reset chosen movie and add title to list of guessed movies
     if (chosenMovie.value.title === movie.title) {
-      chosenMovie.value = null
       guessedMovies.value.push(movie.title)
       return
     }
 
     // If clicked incorrect movie
     if (chosenMovie.value.title !== movie.title) {
-      chosenMovie.value = null
       console.log('wrong')
     }
   }
--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,6 +1,10 @@
 <template>
-  <article :class="[(movie.id === chosenMovie?.id || guessedMovies.includes(movie.title)) ? '' : 'flipped']"
-    class="relative grid preserve-3d duration-700 ease-in-out transform">
+  <article :class="[
+    (
+      movie.id === chosenMovie?.id
+      || movie.id === secondMovie?.id
+      || guessedMovies.includes(movie.title)
+    ) ? '' : 'flipped']" class="relative grid preserve-3d duration-700 ease-in-out transform">
     <div class="w-full h-full col-[1/1] row-[1/1] bg-black"></div>
     <img :src="`src/assets/covers/${movie.cover}.jpg`" :alt="movie.title"
       class="w-full h-full col-[1/1] row-[1/1] object-cover backface-hidden">
@@ -13,6 +17,7 @@ import type { Movie } from '@/types/movie';
 defineProps<{
   movie: Movie
   chosenMovie: Movie | null
+  secondMovie: Movie | null
   guessedMovies: string[]
 }>()
 </script>
