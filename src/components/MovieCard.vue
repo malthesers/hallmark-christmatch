c34b2948dbc7208457a31c7a5dfd02a3e8e49232
malt9830 <malt9830@stud.kea.dk>
2023-12-25 02:06:43 +0100

    Simplified class names for MovieCard style

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,7 +1,7 @@
 <template>
   <article :class="[(movie.id === chosenMovie?.id || guessedMovies.includes(movie.title)) ? '' : 'flipped']"
-    class="relative h-full preserve-3d duration-700 ease-in-out">
-    <div class="absolute w-full h-full bg-black opacity-50"></div>
+    class="relative h-full preserve-3d duration-700 ease-in-out transform">
+    <div class="w-full h-full bg-black"></div>
     <img :src="`src/assets/covers/${movie.cover}.jpg`" :alt="movie.title"
       class="w-full h-full object-cover backface-hidden">
   </article>
@@ -20,18 +20,15 @@ defineProps<{
 <style scoped>
 article {
   transform: perspective(40rem);
+  transform-style: preserve-3d;
+}
+
+img {
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
 }
 
 .flipped {
   transform: rotateY(180deg);
-}
-
-.preserve-3d {
-  transform-style: preserve-3d;
-}
-
-.backface-hidden {
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
 }
 </style>
