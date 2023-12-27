042a4d710acc46839501b88b236099c96bed49ac
malt9830 <malt9830@stud.kea.dk>
2023-12-27 07:24:18 +0100

    Fixed header incorrect GitHub link + added kofi

--- src/components/SantasLittleHeader.vue
+++ src/components/SantasLittleHeader.vue
@@ -2,10 +2,13 @@
   <header class="fixed w-full bg-black bg-opacity-50 text-white py-3 px-4">
     <div class="max-w-3xl mx-auto flex flex-row justify-between">
       <p class="font-title text-3xl">Hallmark Christmatch</p>
-      <nav class="grid place-content-center">
-        <a href="https://github.com/malthesers/soulsborndle" aria-label="github repository link" target="_blank"
-          rel="noopener noreferrer">
-          <Icon icon="mdi:github" width="36" height="36" class="duration-200 hover:opacity-50" />
+      <nav class="flex flex-row gap-2">
+        <a href="https://github.com/malthesers/hallmark-christmatch" aria-label="github repository link" target="_blank"
+          rel="noopener noreferrer" class="duration-200 hover:opacity-50">
+          <Icon icon="mdi:github" width="36" height="36" />
+        </a>
+        <a aria-label="kofi link" target="_blank" rel="noopener noreferrer" class="duration-200 hover:opacity-50">
+          <Icon icon="simple-icons:kofi" width="38" height="38" />
         </a>
       </nav>
     </div>
