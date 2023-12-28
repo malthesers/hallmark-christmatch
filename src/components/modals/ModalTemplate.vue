1df5b508ed85f7cb49685740beb842cd5c968687
malt9830 <malt9830@stud.kea.dk>
2023-12-28 23:21:26 +0100

    Added ModalTemplate

---
+++ src/components/modals/ModalTemplate.vue
@@ -0,0 +1,15 @@
+<template>
+  <Teleport to="body">
+    <Transition name="modal">
+      <aside
+        class="fixed z-20 top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
+        <div class="overflow-auto w-full max-w-xl max-h-[80%] mx-auto bg-black p-4 text-center md:text-lg cursor-auto">
+          <slot></slot>
+        </div>
+      </aside>
+    </Transition>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+</script>
