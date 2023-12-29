883a90340cb33170515a670c3fed2473ae3bd827
malt9830 <malt9830@stud.kea.dk>
2023-12-29 02:41:07 +0100

    Adjusted header title + kofi link

--- src/components/SantasLittleHeader.vue
+++ src/components/SantasLittleHeader.vue
@@ -2,12 +2,13 @@
   <header class="fixed w-full bg-black bg-opacity-50 text-white py-3 px-4">
     <div class="max-w-3xl mx-auto flex flex-row justify-between">
       <p class="font-title text-3xl">Hallmark Christmatch</p>
-      <nav class="flex flex-row gap-2">
+      <nav class="flex flex-row gap-2 items-center">
         <a href="https://github.com/malthesers/hallmark-christmatch" aria-label="github repository link" target="_blank"
           rel="noopener noreferrer" class="duration-200 hover:opacity-50">
           <Icon icon="mdi:github" width="36" height="36" />
         </a>
-        <a aria-label="kofi link" target="_blank" rel="noopener noreferrer" class="duration-200 hover:opacity-50">
+        <a href="https://ko-fi.com/malthesers" aria-label="kofi link" target="_blank" rel="noopener noreferrer"
+          class="duration-200 hover:opacity-50">
           <Icon icon="simple-icons:kofi" width="38" height="38" />
         </a>
       </nav>
