413695ed7bed2fa164a968bd1116b7975d8c2007
malt9830 <malt9830@stud.kea.dk>
2024-01-01 00:08:30 +0100

    Added more 2018 movies

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -2,60 +2,6 @@ import type { Movie } from "@/types/movie";
 
 const allMovies:Movie[] = [
   {
-    title: 'Angel Falls: A Novel Holiday',
-    cover: 'angel-falls-a-novel-holiday',
-    description: 'A book editor must work with a former rival to help a potential writer find her way. He reminds all of them about the Christmas spirit and the value of community.',
-    year: 2019
-  },
-  {
-    title: 'Christmas Under the Stars',
-    cover: 'christmas-under-the-stars',
-    description: 'A Christmas-tree lot helps two people rediscover their holiday spirit.',
-    year: 2019
-  },
-  {
-    title: 'Christmas on My Mind',
-    cover: 'christmas-on-my-mind',
-    description: 'When Lucy wakes up with a wedding dress in hand, she believes that she is late to her wedding with her ex-boyfriend Zach. However, she has no recollection of the past 2 years, including the fact that she is engaged to another man.',
-    year: 2019
-  },
-  {
-    title: 'Merry & Bright',
-    cover: 'merry-and-bright',
-    description: 'Cate\'s mom is setting up dates for her CEO daughter. When she meets Gabe a week before Christmas, she assumes, he\'s a date, but he\'s a corporate recovery consultant for her candy cane company. He\'s also single.',
-    year: 2019
-  },
-  {
-    title: 'Holiday Hearts',
-    cover: 'holiday-hearts',
-    description: 'While planning an annual Christmas party, Peyton is forced together with Ben to care for a friend\'s daughter. While finding their Christmas spirit, will there be some romance along the way.',
-    year: 2019
-  },
-  {
-    title: 'A Blue Ridge Mountain Christmas',
-    cover: 'a-blue-ridge-mountain-christmas',
-    description: 'Hotel manager Willow returns to her stunning Virginia hometown to help her sister plan a Christmas wedding at the inn her family once owned. She must work with current owner and single dad David, who wants to let go of the past.',
-    year: 2019
-  },
-  {
-    title: 'Nostalgic Christmas',
-    cover: 'nostalgic-christmas',
-    description: 'Anne seems to have it all as a driven toy buyer in New York City, but her father\'s retirement signaling the closing of the town\'s beloved toy store brings the city girl back to her small-town roots.',
-    year: 2019
-  },
-  {
-    title: 'Time for You to Come Home for Christmas',
-    cover: 'time-for-you-to-come-home-for-christmas',
-    description: 'A recent widow travels with her son to her Virginia hometown for Christmas. On the way they meet a man who is heading to the same destination. As they continue to spend time together, the pair grows closer.',
-    year: 2019
-  },
-  {
-    title: 'Christmas in Rome',
-    cover: 'christmas-in-rome',
-    description: 'Angela, an American tour guide in Rome, agrees to teach a culture-shocked businessman named Oliver about the heart of Rome at Christmas.',
-    year: 2019
-  },
-  {
     title: 'Angel of Christmas',
     cover: 'angel-of-christmas',
     description: 'After she finally gets the chance to pen her very first article, an aspiring newspaper writer digs into her family history to find the perfect Christmas story.',
@@ -138,6 +84,72 @@ const allMovies:Movie[] = [
     cover: 'christmas-encore',
     description: 'A struggling actress auditions for the lead role in a theater production of \'A Christmas Carol\'.',
     year: 2017
+  },
+  {
+    title: 'Christmas on Honeysuckle Lane',
+    cover: 'christmas-on-honeysuckle-lane',
+    description: 'Emma returns home to celebrate Christmas for the last time with her siblings in the house they grew up in. After their parents\' recent passing, the siblings reluctantly agree to sell the family home on Honeysuckle Lane.',
+    year: 2018
+  },
+  {
+    title: 'Once Upon a Christmas Miracle',
+    cover: 'once-upon-a-christmas-miracle',
+    description: 'This holiday romance is based on the incredible story about organ donor Chris Dempsey and recipient Heather Krueger, who were brought into one another\'s lives by a transplant surgery that led to a miraculous romance.',
+    year: 2018
+  },
+  {
+    title: 'Angel Falls: A Novel Holiday',
+    cover: 'angel-falls-a-novel-holiday',
+    description: 'A book editor must work with a former rival to help a potential writer find her way. He reminds all of them about the Christmas spirit and the value of community.',
+    year: 2019
+  },
+  {
+    title: 'Christmas Under the Stars',
+    cover: 'christmas-under-the-stars',
+    description: 'A Christmas-tree lot helps two people rediscover their holiday spirit.',
+    year: 2019
+  },
+  {
+    title: 'Christmas on My Mind',
+    cover: 'christmas-on-my-mind',
+    description: 'When Lucy wakes up with a wedding dress in hand, she believes that she is late to her wedding with her ex-boyfriend Zach. However, she has no recollection of the past 2 years, including the fact that she is engaged to another man.',
+    year: 2019
+  },
+  {
+    title: 'Merry & Bright',
+    cover: 'merry-and-bright',
+    description: 'Cate\'s mom is setting up dates for her CEO daughter. When she meets Gabe a week before Christmas, she assumes, he\'s a date, but he\'s a corporate recovery consultant for her candy cane company. He\'s also single.',
+    year: 2019
+  },
+  {
+    title: 'Holiday Hearts',
+    cover: 'holiday-hearts',
+    description: 'While planning an annual Christmas party, Peyton is forced together with Ben to care for a friend\'s daughter. While finding their Christmas spirit, will there be some romance along the way.',
+    year: 2019
+  },
+  {
+    title: 'A Blue Ridge Mountain Christmas',
+    cover: 'a-blue-ridge-mountain-christmas',
+    description: 'Hotel manager Willow returns to her stunning Virginia hometown to help her sister plan a Christmas wedding at the inn her family once owned. She must work with current owner and single dad David, who wants to let go of the past.',
+    year: 2019
+  },
+  {
+    title: 'Nostalgic Christmas',
+    cover: 'nostalgic-christmas',
+    description: 'Anne seems to have it all as a driven toy buyer in New York City, but her father\'s retirement signaling the closing of the town\'s beloved toy store brings the city girl back to her small-town roots.',
+    year: 2019
+  },
+  {
+    title: 'Time for You to Come Home for Christmas',
+    cover: 'time-for-you-to-come-home-for-christmas',
+    description: 'A recent widow travels with her son to her Virginia hometown for Christmas. On the way they meet a man who is heading to the same destination. As they continue to spend time together, the pair grows closer.',
+    year: 2019
+  },
+  {
+    title: 'Christmas in Rome',
+    cover: 'christmas-in-rome',
+    description: 'Angela, an American tour guide in Rome, agrees to teach a culture-shocked businessman named Oliver about the heart of Rome at Christmas.',
+    year: 2019
   },
 ]
 
