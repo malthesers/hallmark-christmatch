33709d72c9b5509db3a7666afae0a80d8e50c38a
malt9830 <malt9830@stud.kea.dk>
2023-12-31 09:38:36 +0100

    Adjusted modal text size

--- src/components/modals/InstructionsModal.vue
+++ src/components/modals/InstructionsModal.vue
@@ -1,7 +1,7 @@
 <template>
   <ModalTemplate @closeModal="$emit('closeModal')" :showModal="showInstructionsModal">
     <h2 class="font-title text-5xl mb-2">Instructions</h2>
-    <section class="text-center space-y-2">
+    <section class="text-lg sm:text-xl text-center space-y-2">
       <p>20 cards are laid out in the grid, those being 10 pairs of Hallmark christmas movie covers.</p>
       <p>Start by clicking a card to flip it over and reveal the movie, then click a new card to reveal it and see if they
         match.</p>
--- src/components/modals/VictoryModal.vue
+++ src/components/modals/VictoryModal.vue
@@ -1,7 +1,11 @@
 <template>
   <ModalTemplate @closeModal="$emit('closeModal')" :showModal="showVictoryModal">
     <p class="font-title text-5xl mb-2">Victory!</p>
-    <p>You succesfully matched all the movies in {{ guesses }} guesses.</p>
+    <p class="text-lg sm:text-xl">
+      <span>You succesfully matched all the movies in </span>
+      <span class="font-bold">{{ guesses }}</span>
+      <span> guesses.</span>
+    </p>
   </ModalTemplate>
 </template>
 
