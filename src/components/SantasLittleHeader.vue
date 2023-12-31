1e26af60a808502900a7b21b8db8b75f9b7c05e3
malt9830 <malt9830@stud.kea.dk>
2023-12-31 09:24:09 +0100

    Added Instructions icon button to header

--- src/components/SantasLittleHeader.vue
+++ src/components/SantasLittleHeader.vue
@@ -1,15 +1,18 @@
 <template>
-  <header class="fixed w-full bg-black bg-opacity-30 backdrop-blur-sm text-white py-3 px-4">
+  <header class="fixed w-full bg-black bg-opacity-30 backdrop-blur-sm text-white py-2 px-4">
     <div class="max-w-3xl mx-auto flex flex-row justify-between">
       <p class="font-title text-2xl md:text-3xl pt-2">Hallmark Christmatch</p>
-      <nav class="flex flex-row gap-2 items-center">
+      <nav class="flex flex-row gap-1 sm:gap-2 items-center">
+        <button @click="showInstructionsModal = true" class="duration-200 hover:opacity-50">
+          <Icon icon="mdi:help-circle" class="w-8 h-8 sm:w-9 sm:h-9" />
+        </button>
         <a href="https://github.com/malthesers/hallmark-christmatch" aria-label="github repository link" target="_blank"
           rel="noopener noreferrer" class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:github" width="36" height="36" />
+          <Icon icon="mdi:github" class="w-8 h-8 sm:w-9 sm:h-9" />
         </a>
         <a href="https://ko-fi.com/malthesers" aria-label="kofi link" target="_blank" rel="noopener noreferrer"
           class="duration-200 hover:opacity-50">
-          <Icon icon="simple-icons:kofi" width="38" height="38" />
+          <Icon icon="simple-icons:kofi" class="w-8 h-8 sm:w-[38px] sm:h-[38px] ml-1" />
         </a>
       </nav>
     </div>
