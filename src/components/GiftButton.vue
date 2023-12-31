e8637547afd4b60c83041f4d335f3c12ff5b57ab
malt9830 <malt9830@stud.kea.dk>
2023-12-31 10:09:05 +0100

    Added button component

---
+++ src/components/GiftButton.vue
@@ -0,0 +1,6 @@
+<template>
+  <button
+    class="sm:text-lg text-xl font-semibold px-3 py-2 border-2 border-white duration-200 hover:bg-white hover:text-green-950">
+    <slot></slot>
+  </button>
+</template>
--- src/components/modals/ModalTemplate.vue
+++ src/components/modals/ModalTemplate.vue
@@ -3,9 +3,10 @@
     <Transition name="modal">
       <aside v-if="showModal" @click.self="$emit('closeModal')"
         class="fixed z-20 top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
-        <div
-          class="overflow-auto w-full max-w-lg max-h-[80%] mx-auto bg-green-950 text-white border-2 border-white p-4 pt-6 text-center md:text-lg cursor-auto">
+        <div class="w-full max-w-lg max-h-[80%] mx-auto bg-green-950 text-white border-2 border-white
+          p-4 pt-6 text-center md:text-lg overflow-auto cursor-auto ">
           <slot></slot>
+          <GiftButton @click="$emit('closeModal')" class="mt-4">Close</GiftButton>
         </div>
       </aside>
     </Transition>
