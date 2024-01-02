1869ab7028b910c9dd7f64ba660324a7fce28280
malt9830 <malt9830@stud.kea.dk>
2024-01-02 14:20:59 +0100

    Updated package info

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -5,7 +5,7 @@
       || movie.id === secondMovie?.id
       || guessedMovies.includes(movie.title)
     )
-  }" class=" w-[60px] h-[90px] sm:w-[90px] sm:h-[135px] md:w-[100px] md:h-[150px] grid cursor-pointer duration-300 ease-in-out transform shadow-2xl
+  }" class="w-[60px] h-[90px] sm:w-[90px] sm:h-[135px] md:w-[100px] md:h-[150px] grid cursor-pointer duration-300 ease-in-out transform shadow-2xl
     child:w-full child:h-full child:col-[1/1] child:row-[1/1] child:border-2 child:border-white">
     <div class="bg-green-950 grid">
       <CrownIcon class="w-3/5 h-auto m-auto" />
