1f91bb4d2c41e68223f1878aede0cac2e54b5f5e
malt9830 <malt9830@stud.kea.dk>
2024-01-02 14:28:25 +0100

    Added base to vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hallmark-christmatch/',
   plugins: [vue(), Components({}), AutoImport({ imports: ['vue'], dts: './auto-imports.d.ts' })],
   resolve: {
     alias: {
