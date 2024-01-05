80665983008b73927f300771410816d8e5305fb8
malt9830 <malt9830@stud.kea.dk>
2024-01-05 12:44:13 +0100

    Adjusted header scalability

--- src/components/layout/SantasLittleHeader.vue
+++ src/components/layout/SantasLittleHeader.vue
@@ -1,22 +1,24 @@
 <template>
-  <header class="fixed w-full bg-black bg-opacity-30 backdrop-blur-sm text-white py-2 px-4">
+  <header class="fixed w-full bg-black bg-opacity-30 backdrop-blur-sm text-white sm:py-2 px-3 sm:px-4">
     <div class="max-w-3xl mx-auto flex flex-row justify-between">
+      <!-- Left side -->
       <p class="font-title text-2xl md:text-3xl pt-2">Hallmark Christmatch</p>
-      <nav class="flex flex-row gap-1 sm:gap-2 items-center">
+      <!-- Right side -->
+      <nav class="flex flex-row gap-[2px] sm:gap-1 items-center">
         <button @click="startGame(500)" aria-label="open instructions button" class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:refresh-circle" class="w-8 h-8 sm:w-9 sm:h-9" />
+          <Icon icon="mdi:refresh-circle" class="w-7 h-7 sm:w-9 sm:h-9" />
         </button>
         <button @click="showInstructionsModal = true" aria-label="open instructions button"
           class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:help-circle" class="w-8 h-8 sm:w-9 sm:h-9" />
+          <Icon icon="mdi:help-circle" class="w-7 h-7 sm:w-9 sm:h-9" />
         </button>
         <a href="https://github.com/malthesers/hallmark-christmatch" aria-label="github repository link" target="_blank"
           rel="noopener noreferrer" class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:github" class="w-8 h-8 sm:w-9 sm:h-9" />
+          <Icon icon="mdi:github" class="w-7 h-7 sm:w-9 sm:h-9" />
         </a>
         <a href="https://ko-fi.com/malthesers" aria-label="kofi link" target="_blank" rel="noopener noreferrer"
           class="duration-200 hover:opacity-50">
-          <Icon icon="simple-icons:kofi" class="w-8 h-8 sm:w-[38px] sm:h-[38px] ml-1" />
+          <Icon icon="simple-icons:kofi" class="w-7 h-7 sm:w-[38px] sm:h-[38px] ml-1" />
         </a>
       </nav>
     </div>
