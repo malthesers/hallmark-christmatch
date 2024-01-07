e944e8b5a8017cd0608515d69e968e5448062181
malt9830 <malt9830@stud.kea.dk>
2024-01-07 14:26:01 +0100

    Added potential slide shine effect

--- src/components/CardShine.vue
+++ src/components/CardShine.vue
@@ -1,3 +1,22 @@
 <template>
-  <div class="w-full h-full bg-white bg-opacity-50"></div>
+  <div class="relative w-full h-full overflow-hidden">
+    <div class="absolute w-full h-full bg-gradient-to-r from-transparent via-white/50 to-transparent slide">
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.slide {
+  animation: slide 1000ms linear infinite;
+}
+
+@keyframes slide {
+  0% {
+    left: -100%;
+  }
+
+  100% {
+    left: 100%;
+  }
+}
+</style>
