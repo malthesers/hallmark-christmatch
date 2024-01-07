75a49e0c3105d1989753dc30505ecb6fd7c2b50b
malt9830 <malt9830@stud.kea.dk>
2024-01-07 17:35:25 +0100

    Updated header for mobile

--- src/components/layout/SantasLittleHeader.vue
+++ src/components/layout/SantasLittleHeader.vue
@@ -1,29 +1,29 @@
 <template>
-  <header class="fixed z-10 w-full bg-black bg-opacity-30 backdrop-blur-sm text-white sm:py-2 px-3 sm:px-4">
+  <header class="fixed z-10 w-full bg-black bg-opacity-30 backdrop-blur-sm text-white p-2">
     <div class="max-w-3xl mx-auto flex flex-col xs:flex-row justify-between">
       <!-- Left side -->
-      <p class="font-title text-2xl md:text-3xl pt-2">Hallmark Christmatch</p>
+      <p class="font-title text-3xl text-center xs:text-left pt-2">Hallmark Christmatch</p>
       <!-- Right side -->
-      <nav class="flex flex-row gap-[2px] sm:gap-1 items-center">
+      <nav class="flex flex-row gap-[2px] xs:gap-1 place-content-center xs:items-center">
         <!-- Restart button -->
         <button @click="startGame(500)" aria-label="open instructions button"
           :class="{ 'opacity-50 pointer-events-none': disableRestart }" class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:refresh-circle" class="w-7 h-7 sm:w-9 sm:h-9" />
+          <Icon icon="mdi:refresh-circle" class="w-9 h-9" />
         </button>
         <!-- Instructions button -->
         <button @click="showInstructionsModal = true" aria-label="open instructions button"
           class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:help-circle" class="w-7 h-7 sm:w-9 sm:h-9" />
+          <Icon icon="mdi:help-circle" class="w-9 h-9" />
         </button>
         <!-- GitHub link -->
         <a href="https://github.com/malthesers/hallmark-christmatch" aria-label="github repository link" target="_blank"
           rel="noopener noreferrer" class="duration-200 hover:opacity-50">
-          <Icon icon="mdi:github" class="w-7 h-7 sm:w-9 sm:h-9" />
+          <Icon icon="mdi:github" class="w-9 h-9" />
         </a>
         <!-- Ko-fi link -->
         <a href="https://ko-fi.com/malthesers" aria-label="kofi link" target="_blank" rel="noopener noreferrer"
           class="duration-200 hover:opacity-50">
-          <Icon icon="simple-icons:kofi" class="w-7 h-7 sm:w-[38px] sm:h-[38px] ml-1" />
+          <Icon icon="simple-icons:kofi" class="w-[38px] h-[38px] ml-1" />
         </a>
       </nav>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {
       screens: {
-        'xs': '420px'
+        'xs': '430px'
       }
     },
   },
